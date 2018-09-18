6eba815106dbb80b9b22819eb75ce62086e3df85
srikutty19 <sri9132@gmail.com>
2018-09-18 15:45:48 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 var krms_config ={			
-	'ApiUrl':"",	
-	'DialogDefaultTitle':"KMRS",	
-	'pushNotificationSenderid':"",	
-	'facebookAppId':"", 
-	'APIHasKey':""
+	'ApiUrl':"http://kdboys.chiarro.com/mobileapp/api",	
+	'DialogDefaultTitle':"Kovai Delivery App",	
+	'pushNotificationSenderid':"465251440510",	
+	'facebookAppId':"273381436604674", 
+	'APIHasKey':"fdc520d2bb339d76e8c95bc0f2432538"
 };

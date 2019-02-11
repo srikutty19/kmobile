7bb3ac14a7ddcc7f5c14d3fe7f2c8dcc36fac04d
srikutty19 <sri9132@gmail.com>
2019-02-11 12:15:39 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 var krms_config ={			
-	'ApiUrl':"http://kdboys.chiarro.com/mobileapp/api",	
-	'DialogDefaultTitle':"Kovai Delivery App",	
+	'ApiUrl':"http://rapidtest.ga/foodieschoice/mobileapp/api",	
+	'DialogDefaultTitle':"Foodies App",	
 	'pushNotificationSenderid':"465251440510",	
 	'facebookAppId':"273381436604674", 
 	'APIHasKey':"fdc520d2bb339d76e8c95bc0f2432538"
